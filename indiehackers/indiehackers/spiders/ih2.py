3ff72ed0210b6cb56049c0142f84cbdac1f2683e
NeuTokio-3 <47566604+neotokio@users.noreply.github.com>
2019-07-26 13:20:47 +0200

    Update ih2.py

--- indiehackers/indiehackers/spiders/ih2.py
+++ indiehackers/indiehackers/spiders/ih2.py
@@ -60,34 +60,3 @@ class altSpider(CrawlSpider):
         item['City'] = response.xpath('//div[contains(@class, "user-header__metadata")]//p[contains(@class, "user-header__metadata-item")and contains(., ",")]//text()').extract() or 'None'
         item['Age'] = response.xpath('//div[contains(@class, "user-header__metadata")]//p[contains(@class, "user-header__metadata-item")and not(contains(., "joined"))]//text()').re('[0-9]+') or 'None'
         yield item
-        
-        
-        #item['BusinessType'] = texts[3] if len(texts) >= 4 else 'None'
-        #item['BusinessArea'] = texts[4] if len(texts) >= 5 else 'None'
-        #item['CreatorName'] = response.xpath('.//div[contains(@class, "product__content")]//div[contains(@class, "product-sidebar__users")]//div[contains(@class, "user-link")]//a//span[contains(@class, "user-link__name")]//text()').extract()
-        #or item ['CreatorNameFromProfilePage'] = response.xpath('//div[contains(@class, "user-header")]//h1[contains(@class, "user-header__username")]//text()').extract()
-        #item['LinkToProfile'] = response.xpath('.//div[contains(@class, "product__content")]//div[contains(@class, "product-sidebar__users")]//div[contains(@class, "user-link")]//a//@href').extract()
-        #item['CreatorTwitter'] = response.xpath('//div[contains(@class, "user-header__satellites")]//a[contains(@class, "user-header__satellite")]//@href').extract()[1] or [2] = item['CreatorMail']
-        #item['AgeCityJoinDate'] = response.xpath('//div[contains(@class, "user-header__metadata")]//p[contains(@class, "user-header__metadata-item")]//text()').extract()
-        #item['Followers'] = response.xpath('//div[contains(@class, "user-sidebar")]//section[contains(@class, "user-stats")]//div[contains(@class, "user-stats__stat")]//span[contains(@class, "user-stats__number")]//text()').extract()[0] or [1] = item['Points']
-        #item['Following'] response.xpath('//div[contains(@class, "user-sidebar")]//section[contains(@class, "following")]//span[contains(@class, "user-sidebar__section-supplement")]//text()').extract()
-        
-#        >>> response.xpath('//div[contains(@class, "user-header__metadata")]//p[contains(@class, "user-header__metadata-item")]//text()').extract()[0]
-#'\n                  31\n                  \n                '
-#>>> response.xpath('//div[contains(@class, "user-header__metadata")]//p[contains(@class, "user-header__metadata-item")]//text()').extract()[1]
-#'Singapore, Singapore'
-#>>> response.xpath('//div[contains(@class, "user-header__metadata")]//p[contains(@class, "user-header__metadata-item")]//text()').extract()[2]
-#'\n                joined 2 years ago\n              '
-
-#response.xpath('//div[contains(@class, "user-header__metadata")]//p[contains(@class, "user-header__metadata-item")and contains(., "joined")]//text()').extract()
-#https://www.indiehackers.com/moe?id=CAMrajTi60QMSXJZqp0sG7IptAH2 - CITY + JOINED, no age
-#https://www.indiehackers.com/levelsio?id=FfqtvHZDDnebVFkWD8hvZmWZVJJ2 - JOINED, no city
-#https://www.indiehackers.com/johnonolan?id=TTNpIuNSsnNlltrLHqLaQMzeHxa2 - ALL
-#response.xpath('//div[contains(@class, "user-header__metadata")]//p[contains(@class, "user-header__metadata-item")and contains(., "joined")]//text()').extract()
-#Visit https://www.indiehackers.com/products > Click Log-in button > Render form > enter to input id > Load products page > infinite scroll
-#No need to play with cookies, let scrapy handle it
-#Make Scrapy click button(DONE!), navigate to form rendered(partialy?), fill form and send, render /products page, make scroll
-#response.xpath('.//div[contains(@class, "product-header__content")]//h1[contains(@class, "product-header__title")]//p//text()').extract()
-
-#scrapy shell 'http://localhost:8050/render.html?url=https://www.indiehackers.com/product/worfor&timeout=10&wait=7.5'
-#response.xpath('//div[contains(@class, "product-metrics__content")]//a[contains(@class, "product-metrics__stat--revenue")]//span[contains(@class, "product-metrics__stat-number")]//text()').extract()
